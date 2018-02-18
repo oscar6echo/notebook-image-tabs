dce3d2998306184d5bc735c70ede9a2c3e63741e
oscar6echo <olivier.borderies@gmail.com>
2018-02-19 00:47:11 +0100

    Add save method

--- notebook_image_tabs/viewer.py
+++ notebook_image_tabs/viewer.py
@@ -1,5 +1,6 @@
 
 import os
+import datetime as dt
 
 from IPython.display import display, HTML
 
@@ -31,3 +32,24 @@ class ImageTabs:
         display image tabs
         """
         display(HTML(self.html))
+
+    def save(self,
+             folder='dump',
+             name=None,
+             tagged=True):
+        """
+        save as file
+        """
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        if not name:
+            name = 'myplot'
+        tag = ''
+        if tagged:
+            tag = dt.datetime.now().strftime('_%Y%m%d_%H%M%S')
+        suffix = '.html'
+        filename = '{}{}{}'.format(name, tag, suffix)
+        path = os.path.join(folder, filename)
+
+        with open(path, 'w') as f:
+            f.write(self.html)
